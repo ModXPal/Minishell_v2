e10bcc37ad1187359d2dcf498555c21d9ed92d01
Vincent BACHELET <vbachele@e3r4p16.42.fr>
2021-10-18 15:54:27 +0200

    version sans CDPATH

--- src/builtin/ft_cd_utils_3.c
+++ src/builtin/ft_cd_utils_3.c
@@ -6,19 +6,19 @@
 /*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:41:03 by vbachele          #+#    #+#             */
-/*   Updated: 2021/10/18 15:44:47 by vbachele         ###   ########.fr       */
+/*   Updated: 2021/10/18 15:53:59 by vbachele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-int	check_cdpath_exists(t_var *var)
-{
-	char *cdpath;
+// int	check_cdpath_exists(t_var *var)
+// {
+// 	char *cdpath;
 	
-	cdpath = 0;
-	if (ft_envar_find_content(var->envar, "CDPATH")
-	{
-		cdpath = 
-	}
-}
+// 	cdpath = 0;
+// 	if (ft_envar_find_content(var->envar, "CDPATH")
+// 	{
+// 		cdpath = 
+// 	}
+// }
