5027c2da606dff6108bd4edafaba9c2c9b891297
Robin COLLAS <rcollas@e3r8p15.clusters.42paris.fr>
2021-10-20 18:20:32 +0200

    fixing multipipes

--- src/multipipes/exec.c
+++ src/multipipes/exec.c
@@ -6,7 +6,7 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 23:16:49 by rcollas           #+#    #+#             */
-/*   Updated: 2021/10/20 00:34:29 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/10/20 18:19:07 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	close_pipes(t_pvar *pvar, int **pipefd)
 	int	j;
 
 	j = -1;
-	while (++j < pvar->cmd_nb)
+	while (++j < pvar->cmd_nb + 1)
 	{
 		if (close(pipefd[j][0]) == -1)
 		{
@@ -44,10 +44,11 @@ int	check_access(t_pvar *pvar, int i)
 	return (-1);
 }
 
-int	cmd_not_found(t_var *var)
+int	cmd_not_found(t_var *var, t_pvar *pvar)
 {
 	write (2, var->input->cmd, ft_strlen(var->input->cmd));
 	write (2, ": command not found\n", 20);
+	free(pvar->cmd);
 	return (0);
 }
 
@@ -62,7 +63,7 @@ int	get_cmds(t_pvar *pvar, t_var *var)
 		if (check_access(pvar, i) == SUCCESS)
 			break ;
 		else if (check_access(pvar, i) == FAIL)
-			return (cmd_not_found(var));
+			return (cmd_not_found(var, pvar));
 		free(pvar->cmd);
 	}
 	return (1);
@@ -100,19 +101,19 @@ void	proceed_pipes(t_pvar *pvar, t_var *var, int **pipefd, int i)
 	if (i == 0)
 	{
 		if (get_cmds(pvar, var) == FAIL)
-			return ;
+			exit (1);
 		first_cmd(pvar, var, pipefd, i);
 	}
 	else if (i == pvar->cmd_nb - 1)
 	{
 		if (get_cmds(pvar, var) == FAIL)
-			return ;
+			exit (1);
 		last_cmd(pvar, var, pipefd, i);
 	}
 	else
 	{
 		if (get_cmds(pvar, var) == FAIL)
-			return ;
+			exit (1);
 		in_between_cmd(pvar, var, pipefd, i);
 	}
 }
@@ -125,7 +126,7 @@ int	exec(t_pvar *pvar, t_var *var, int **pipefd, pid_t *pids)
 
 	i = -1;
 	start = var->input;
-	while (i++ < pvar->cmd_nb)
+	while (++i < pvar->cmd_nb)
 	{
 		if (i > 0)
 			var->input = var->input->next;
@@ -144,7 +145,9 @@ int	exec(t_pvar *pvar, t_var *var, int **pipefd, pid_t *pids)
 	close_pipes(pvar, pipefd);
 	i = -1;
 	while (++i < pvar->cmd_nb)
-		waitpid(pids[i], &status, 0);
+		waitpid(0, &status, 0);
+	//free(pids);
 	var->input = start;
+	free_split(pvar->path);
 	return (1);
 }
